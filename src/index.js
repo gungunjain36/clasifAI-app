9296dbd7727cea6fbbee816f4c607cb609b6c76c
gungunjain36 <jgungun1723@gmail.com>
2024-07-10 13:53:39 +0530

    database connection setup

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// require('dotenv').config({path: './env'})
+// import { configDotenv } from "dotenv";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config()
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+;( async ()=> {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}`/ `${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR: ",error)
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port
+                ${process.env.PORT}`)
+        }
+    }catch(error){
+        console.error("ERROR: ",error)
+        throw err
+    }
+})()
+
+*/
